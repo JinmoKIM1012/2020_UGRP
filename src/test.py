a8546b7519f9cee42dddd21c8dbd23c8937d705d
JinmoKIM1012 <jmkim1012@postech.ac.kr>
2020-07-29 03:56:13 +0900

    Travis Test
    Travis Test

--- src/test.py
+++ src/test.py
@@ -3,3 +3,11 @@ print('Slack Test')
 print('PR test')
 print('PR test by SEOYEONG')
 print('travis ci test2')
+
+
+def pytest__():
+    return 'Hello, pytest!'
+
+
+def test_pytest__():
+    assert pytest__() == 'Hello, pytest!'
